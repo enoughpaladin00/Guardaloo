5a2a6729b386bce1429bbfcc3479a958a7ded836
simo2906 <simone.p02@libero.it>
2025-05-27 14:25:58 +0200

    patch 0.0.5

--- build/app/javascript/slider.js
+++ build/app/javascript/slider.js
@@ -4,14 +4,30 @@ document.addEventListener('turbo:load', () => {
     const btnLeft = slider.querySelector('.slider-btn.left');
     const btnRight = slider.querySelector('.slider-btn.right');
 
-    if (track && btnLeft && btnRight) {
-      btnRight.addEventListener('click', () => {
-        track.scrollBy({ left: 220, behavior: 'smooth' });
-      });
+    if (!track || !btnLeft || !btnRight) return;
 
-      btnLeft.addEventListener('click', () => {
-        track.scrollBy({ left: -220, behavior: 'smooth' });
-      });
-    }
+    const updateButtons = () => {
+      const scrollLeft = track.scrollLeft;
+      const maxScroll = track.scrollWidth - track.clientWidth;
+
+      btnLeft.style.display = scrollLeft > 0 ? 'block' : 'none';
+
+      btnRight.style.display = scrollLeft < maxScroll ? 'block' : 'none';
+    };
+
+    track.addEventListener('scroll', updateButtons);
+    window.addEventListener('resize', updateButtons);
+    updateButtons();
+
+    
+    btnRight.addEventListener('click', () => {
+      track.scrollBy({ left: 1000, behavior: 'smooth' });
+    });
+    btnLeft.addEventListener('click', () => {
+      track.scrollBy({ left: -1000, behavior: 'smooth' });
+    });
+    
+
+    
   });
 });
