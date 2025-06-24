07f0cddee3ac1dcb434d83281b7d7f55d21c7e05
Davide Ricci <riccid523@gmail.com>
2025-06-24 12:35:12 +0200

    v-0.0.10 user popup menu

--- build/app/javascript/navbar.js
+++ build/app/javascript/navbar.js
@@ -1,16 +1,5 @@
-document.querySelectorAll(".profile-svg").forEach(function(el) {
-    el.addEventListener("click", function() {
-        const profileMenu = document.getElementById("profile-menu");
-        const currentDisplay = window.getComputedStyle(profileMenu).display;
-
-        if (currentDisplay === "none") {
-            profileMenu.style.display = "flex";
-        } else {
-            profileMenu.style.display = "none";
-        }
-    });
-});
-
+/*javascript for popup menu on navbar*/
+/*hamburger menu*/
 document.addEventListener("turbo:load", () => {
   const hamburgerTrigger = document.getElementById("hamburgerMenuTrigger");
   const hamburgerMenu = document.getElementById("hamburgerMenu");
@@ -27,6 +16,28 @@ document.addEventListener("turbo:load", () => {
     setTimeout(() => {
       if (!hamburgerMenu.matches(':hover')) {
         hamburgerMenu.style.display = "none";
+      }
+    }, 200);
+  });
+});
+
+/*user menu*/ 
+document.addEventListener("turbo:load", () =>{
+  const userTrigger = document.getElementById("userMenu-Trigger");
+  const userMenu = document.getElementById("user-menu");
+
+  userTrigger.addEventListener("mouseenter", () =>{
+    userMenu.style.display = "block";
+  });
+
+  userMenu.addEventListener("mouseleave", () => {
+    userMenu.style.display = "none";
+  });
+
+  userTrigger.addEventListener("mouseleave", () => {
+    setTimeout(() => {
+      if (!userMenu.matches(':hover')) {
+        userMenu.style.display = "none";
       }
     }, 200);
   });
