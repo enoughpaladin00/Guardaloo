6ca6b67d4f3519ea530536ca2446137ae1e86442
alesavo <savo.2021372@studenti.uniroma1.it>
2025-07-08 13:22:19 +0200

    Aggiunto filtraggio e ricerca di cinema per città

--- build/app/controllers/cinemas_controller.rb
+++ build/app/controllers/cinemas_controller.rb
@@ -1,26 +1,44 @@
-
-
 class CinemasController < ApplicationController
   before_action :authenticate_user!
-  skip_before_action :authenticate_user!, only: [:programmazione] 
-
   require 'httparty'
 
   def index
-    all_cinemas = Cinemasdef.all 
-    
-    if params[:lat].present? && params[:lon].present?
+    all_cinemas = Cinemasdef.all
+    cinemas_to_render = []
+    search_range = params[:range].present? ? params[:range].to_f : 10.0
+    search_range = 10.0 if search_range <= 0 
+
+    if params[:city].present?
+      # per la ricerca per città
+      city_name = params[:city].strip
+      # Per trovare i cinema nella città inserita 
+      filtered_cinemas_by_city = all_cinemas.where("lower(town) = ?", city_name.downcase)
+      cinemas_to_render = filtered_cinemas_by_city.map do |cinema|
+        is_favorited = current_user ? current_user.favorite_cinemas_list.include?(cinema) : false
+        {
+          id: cinema.id,
+          name: cinema.name,
+          address: cinema.address,
+          town: cinema.town,
+          province: cinema.province,
+          phone: cinema.phone,
+          distance: nil, 
+          is_favorited: is_favorited 
+        }
+      end.compact.sort_by { |c| c[:name] }
+      
+    elsif params[:lat].present? && params[:lon].present?
       user_lat = params[:lat].to_f
       user_lon = params[:lon].to_f
+      
       
       user_city = "Sconosciuta" 
       user_province = "Sconosciuta" 
       user_country = "Italy"
-
+      
       begin
         nominatim_url = "https://nominatim.openstreetmap.org/reverse?format=json&lat=#{user_lat}&lon=#{user_lon}"
-        nominatim_response = HTTParty.get(nominatim_url, headers: {"User-Agent" => "YourAppName/1.0 (your_email@example.com)"}) 
-        
+        nominatim_response = HTTParty.get(nominatim_url, headers: {"User-Agent" => "GuardalooApp/1.0 (guardaloo@example.com)"}) 
         if nominatim_response.success?
           geocode_data = nominatim_response.parsed_response
           user_city = geocode_data['address']['city'] || geocode_data['address']['town'] || geocode_data['address']['village'] || "Frosinone"
@@ -37,23 +55,16 @@ class CinemasController < ApplicationController
         Rails.logger.error "Errore sconosciuto nella geocodifica inversa: #{e.message}"
       end
 
-     
-      if current_user
-        favorited_cinema_ids = current_user.favorite_cinemas_list.pluck(:id)
-      else
-        favorited_cinema_ids = []
-      end
 
-      cinemas_in_range = all_cinemas.map do |cinema|
+      favorited_cinema_ids = current_user ? current_user.favorite_cinemas_list.pluck(:id) : []
+      cinemas_to_render = all_cinemas.map do |cinema|
         cinema_lat = cinema.lat.to_f rescue nil 
         cinema_lon = cinema.lon.to_f rescue nil 
         
         if cinema_lat && cinema_lon
           distance = distance_km(user_lat, user_lon, cinema_lat, cinema_lon)
-          
           is_favorited = favorited_cinema_ids.include?(cinema.id)
-          
-          if distance < 10.0 
+          if distance <= search_range 
             {
               id: cinema.id,
               name: cinema.name,
@@ -72,12 +83,12 @@ class CinemasController < ApplicationController
         end
       end.compact.sort_by { |c| c[:distance] } 
     else
-      cinemas_in_range = [] 
+      cinemas_to_render = [] 
     end
 
     respond_to do |format|
-      format.html { @cinemas = cinemas_in_range } 
-      format.json { render json: cinemas_in_range } 
+      format.html { @cinemas = cinemas_to_render } 
+      format.json { render json: cinemas_to_render } 
     end
   end
 
