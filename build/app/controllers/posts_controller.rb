31aa435e0055bdf646ed5be9bb36dc9708cd71c5
enoughpaladin00 <enoughpaladin00@gmail.com>
2025-07-14 14:34:15 +0200

    later fixes

--- build/app/controllers/posts_controller.rb
+++ build/app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, except: [ :index, :show ]  # Richiede login tranne per index/show
+  before_action :authenticate_user!
   before_action :set_post, only: [ :show, :edit, :update, :destroy ]
   before_action :authorize_user!, only: [ :edit, :update, :destroy ]  # Autorizza solo il proprietario
 
